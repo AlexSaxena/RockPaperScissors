d0cddb477e3d0ad2912ee1761c48d4ccff766a92
AlexSaxena <saxenaalex@gmail.com>
2019-09-30 21:27:09 +0200

    Adds puppeteer capabilities to World.js, adds first step with puppeteer

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -1,10 +1,22 @@
-const { Given, Then } = require("cucumber");
+const { Given, Then, When } = require("cucumber");
 
-Given('I visit the site', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
-  Then('I should see {string}', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+Given('I visit the landing page', function () {
+  return this.openHomePage();
+
+});
+
+Then('I should see {string}', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I should see {string} button', function (string) {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
+Given('I click on {string} button', function (string) {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
