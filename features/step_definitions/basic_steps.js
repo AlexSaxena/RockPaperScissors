9b83607ade8cd9d07d36acb9d045a3b1efd7b298
AlexSaxena <saxenaalex@gmail.com>
2019-09-30 21:38:31 +0200

    Adds h1 test header in Index, Adds second feature test, Adds content check and button click in puppeteer World.js

--- features/step_definitions/basic_steps.js
+++ features/step_definitions/basic_steps.js
@@ -5,9 +5,8 @@ Given('I visit the landing page', function () {
 
 });
 
-Then('I should see {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('I should see {string}', function (content) {
+  return this.pageHasTextContent(content);
 });
 
 Then('I should see {string} button', function (string) {
